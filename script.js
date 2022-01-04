1c5a8d567b3719bd13e4269444850c9e719417d5
Sasha Mainardi <sashamainardi4@gmail.com>
2022-01-04 21:05:42 +0100

    first two dropdowns

--- script.js
+++ script.js
@@ -14,10 +14,67 @@ window.onscroll = function() {
 
 //  DELETE USELESS PROPERTIES 
 
-  const features = document.getElementById("dropdownMenuButton");
-  const featMenu = document.getElementById("features-menu");
+//   const features = document.getElementById("dropdownMenuButton");
+//   const featMenu = document.getElementById("features-menu");
 
-  features.addEventListener("click" , function(){
-   featMenu.style.transform = "none";
-   featMenu.style = "position:absolute; left:0px; top:0px right:0px bottom: 0px;";
-  });
+//   features.addEventListener("click" , function(){
+//    featMenu.style.transform = "none";
+//    featMenu.style = "position:absolute; left:0px; top:0px right:0px bottom: 0px;";
+//   });
+
+
+const btnFeat = document.getElementById("btn-features");
+const dropdown = document.getElementById("menu-features");
+const backFeat = document.getElementById("back-features");
+const tools = document.getElementById("btn-tools");
+const toolsMenu = document.getElementById("menu-tools")
+const company = document.getElementById("btn-company");
+const companyMenu = document.getElementById("menu-company");
+const custumer = document.getElementById("btn-custumers");
+const custumerMenu = document.getElementById("menu-custumer")
+
+
+btnFeat.addEventListener("click" , function (){
+    dropdown.style = "background-clip: border-box";
+    
+}
+);
+
+btnFeat.addEventListener("click" , function (){
+    dropdown.style.display = "block";
+}
+);
+
+backFeat.addEventListener("click" , function(){
+   dropdown.style.display = "none";
+});
+
+
+
+
+
+tools.addEventListener("click" , function (){
+    toolsMenu.style.display = "block";    
+}
+);
+
+
+
+tools.addEventListener("click" , function (){
+    toolsMenu.style.display = "block";    
+}
+);
+
+
+company.addEventListener("click" , function (){
+    companyMenu.style.display = "block";    
+}
+);
+
+
+custumer.addEventListener("click" , function (){
+    custumerMenu.style.display = "block";    
+}
+);
+
+
